2993348b63b1a20f3dff450083e054d2d98fcd55
dedywd <dedydarmawan2848@gmail.com>
2019-04-06 22:48:18 +0800

    app commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator';
+import Menu from './src/Quiz/Menu';
+
+//All source code copyrighted by Dedy Darmawan
+export default class App extends React.Component {
+
+  render() {
+      return (
+
+        <Menu />
+      );
+    }
+  }
